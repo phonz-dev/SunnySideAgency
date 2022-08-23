88953ddf38ea94ab246ed0314503f5d90555671d
Alphonzo Escolar <phonz.dev@gmail.com>
2022-08-23 18:46:02 +0800

    Add background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,43 @@ module.exports = {
     },
     colors: {
       // Primary
-      'soft-red': 'hsl(7, 99%, 70%)',
-      'yellow': 'hsl(51, 100%, 49%)',
-      'dark-desaturated-cyan': 'hsl(167, 40%, 24%)',
-      'dark-blue': 'hsl(198, 62%, 26%)',
-      'dark-moderate-cyan': 'hsl(168, 34%, 41%)',
+      'soft-red': '#fe7867',
+      'yellow': '#fad400',
+      'dark-desaturated-cyan': '#25564b',
+      'dark-blue': '#19536b',
+      'dark-moderate-cyan': '#458c7e',
       // Neutral
-      'very-dark-desaturated-blue': 'hsl(212, 27%, 19%)',
-      'very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
-      'dark-grayish-blue': 'hsl(232, 10%, 55%)',
-      'white': 'hsl(0, 0%, 100%)'
+      'very-dark-desaturated-blue': '#23303e',
+      'very-dark-grayish-blue': '#5a636c',
+      'dark-grayish-blue': '#818498',
+      'grayish-blue': '#a7abae'
     },
     fontFamily: {
       barlow: ['Barlow', 'sans-serif'],
       fraunces: ['Fraunces', 'serif'],
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'img-header-mb': "url('../images/mobile/image-header.jpg')",
+        'img-header-des': "url('../images/desktop/image-header.jpg')",
+        'img-transform-mb': "url('../images/mobile/image-transform.jpg')",
+        'img-transform-des': "url('../images/desktop/image-transform.jpg')",
+        'img-standout-mb': "url('../images/mobile/image-stand-out.jpg')",
+        'img-standout-des': "url('../images/desktop/image-stand-out.jpg')",
+        'img-graphic-mb': "url('../images/mobile/image-graphic-design.jpg')",
+        'img-graphic-des': "url('../images/desktop/image-graphic-design.jpg')",
+        'img-photography-mb': "url('../images/mobile/image-photography.jpg')",
+        'img-photography-des': "url('../images/desktop/image-photography.jpg')",
+        'img-cone-mb': "url('../images/mobile/image-gallery-cone.jpg')",
+        'img-cone-des': "url('../images/desktop/image-gallery-cone.jpg')",
+        'img-milk-mb': "url('../images/mobile/image-gallery-milkbottles.jpg')",
+        'img-milk-des': "url('../images/desktop/image-gallery-milkbottles.jpg')",
+        'img-orange-mb': "url('../images/mobile/image-gallery-orange.jpg')",
+        'img-orange-des': "url('../images/desktop/image-gallery-orange.jpg')",
+        'img-sugarcubes-mb': "url('../images/mobile/image-gallery-sugarcubes.jpg')",
+        'img-sugarcubes-des': "url('../images/desktop/image-gallery-sugarcubes.jpg')",
+      }
+    },
   },
   plugins: [],
 }
